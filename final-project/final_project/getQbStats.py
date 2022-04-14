97c8ff58e3256d8b599820dfc9317f7c1257ff69
Ram Damodaran <dramvignesh@gmail.com>
2022-04-13 23:07:54 -0500

    stats from api

--- final-project/final_project/getQbStats.py
+++ final-project/final_project/getQbStats.py
@@ -4,13 +4,106 @@ import requests
 import re
 import csv
 
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.pro-football-reference.com/players/W/WilsRu00.htm"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+# Get table
+table = soup.find(class_="table_outer_container")
+
+# Get head
+thead = table.find('thead')
+th_head = thead.find_all('th')
+
+for thh in th_head:
+    # Get case value
+    print(thh.get_text())
+
+    # Get data-stat value
+    print(thh.get('data-stat'))
+
+# Get body
+tbody = table.find('tbody')
+tr_body = tbody.find_all('tr')
+
+for trb in tr_body:
+    # Get id
+    print(trb.get('id'))
+
+    # Get th data
+    th = trb.find('th')
+    print(th.get_text())
+    print(th.get('data-stat'))
+
+    for td in trb.find_all('td'):
+        # Get case value
+        print(td.get_text())
+        # Get data-stat value
+        print(td.get('data-stat'))
+
+# Get footer
+tfoot = table.find('tfoot')
+thf = tfoot.find('th')
+
+# Get case value
+print(thf.get_text())
+# Get data-stat value
+print(thf.get('data-stat'))
+
+for tdf in tfoot.find_all('td'):
+    # Get case value
+    print(tdf.get_text())
+    # Get data-stat value
+    print(tdf.get('data-stat'))
+
+"""
+
 url = "https://www.pro-football-reference.com/players/W/WilsRu00.htm"
 
 data = requests.get(url)
 html = BeautifulSoup(data.text, 'html.parser')
 
-content = str(html.find("tr", {"class":"full_table"}))
-size = len("\"<tr class=\"full_table\" id=\"passing.\"")
-year = content[size:]
-print(content)
+#content = str(html.find("tr", {"class":"full_table"}))
+#size = len("\"<tr class=\"full_table\" id=\"passing.\"")
+#year = content[size:]
+#year = content[:10]
 
+
+id = "passing.2018"
+content = html.find("tr", {"id": id})
+pass_cmp = html.find("data-stat")
+pass_att = content.find("data-stat",{"id": "pass_att"})
+pass_cmp_perc = content.find("data-stat",{"id": "pass_cmp_perc"})
+pass_yds = content.find("data-stat",{"id": "pass_yds"})
+pass_td = content.find("data-stat",{"id": "pass_td"})
+pass_td_perc = content.find("data-stat",{"id": "pass_td_perc"})
+pass_int = content.find("data-stat",{"id": "pass_int"})
+pass_int_perc = content.find("data-stat",{"id": "pass_int_perc"})
+pass_yds_per_att = content.find("data-stat",{"id": "pass_yds_per_att"})
+pass_rating = content.find("data-stat",{"id": "pass_rating"})
+pass_first_down = content.find("data-stat",{"id": "pass_first_down"})
+comebacks = content.find("data-stat",{"id": "comebacks"})
+pass_first_down = content.find("data-stat",{"id": "pass_first_down"})
+awards = content.find("data-stat",{"id": "awards"})
+print(pass_cmp)
+print(pass_att)
+print(pass_cmp_perc)
+print(pass_yds)
+print(pass_td)
+print(pass_td_perc)
+print(pass_int)
+print(pass_int_perc)
+print(pass_yds_per_att)
+print(pass_rating)
+print(pass_first_down)
+print(comebacks)
+print(pass_first_down)
+print(awards)
+
+
+# stats needed - cmp, att, cmp%, yds, td, int, int%, 1D, rate, y/a, gwd, awards
+
+"""
--- final-project/final_project/getQbStatsApi.py
+++ final-project/final_project/getQbStatsApi.py
@@ -1,11 +1,60 @@
 import sportsipy
 from sportsipy.nfl.roster import Player
 from sportsipy.nfl.teams import Teams
+from sportsipy.nfl.roster import Roster
+import pandas as pd
+import csv
 
-print("hello")
+dataset = pd.read_csv("data/salaryData.csv", header=None)
+skippedFirst = False
+stats = []
+for row_index, row in dataset.iterrows():
+   # print(row[0])
+   # print(row[5])
+    if(skippedFirst):
+        names = row[0].split(" ")
+        firstName = names[0]
+        lastName = names[1]
 
-brees = Player('BreeDr00')
-print(brees.name)  # Prints 'Drew Brees'
-print(brees.passing_yards)  # Prints Brees' career passing yards
-# Prints a Pandas DataFrame of all relevant stats per season for Brees
-print(brees.dataframe)
+        number = "00"
+        if(row[0] == "Derek Carr" or row[0] == "Josh Allen"):
+            number = "02"
+        apiId = lastName[0:4] + firstName[0:2] + number
+     #   print(apiId)
+        player = Player(apiId)
+       # print(player.name)  # Prints 'Drew Brees'
+      #  print(player.passing_yards)  # Prints Brees' career passing yards
+        # Prints a Pandas DataFrame of all relevant stats per season for Brees
+        #print(player.dataframe)
+        dates = row[5].split("-")
+        currentYear = int(dates[0])
+        while(currentYear <= int(dates[1])):
+            completed_passes = player(str(currentYear)).completed_passes
+            attempted_passes = player(str(currentYear)).attempted_passes 
+            catch_percentage = player(str(currentYear)).catch_percentage
+            passing_touchdowns = player(str(currentYear)).passing_touchdowns
+            interceptions_thrown = player(str(currentYear)).interceptions_thrown
+            passing_yards_per_attempt = player(str(currentYear)).passing_yards_per_attempt
+            interception_percentage = player(str(currentYear)).interception_percentage
+            quarterback_rating = player(str(currentYear)).quarterback_rating
+            yards_per_game_played = player(str(currentYear)).yards_per_game_played
+            passing_yards_per_attempt = player(str(currentYear)).passing_yards_per_attempt
+            fourth_quarter_comebacks = player(str(currentYear)).fourth_quarter_comebacks
+            passing_yards = player(str(currentYear)).passing_yards
+
+            stats.append([row[0],currentYear,completed_passes,attempted_passes,catch_percentage,passing_touchdowns,interceptions_thrown,passing_yards_per_attempt,interception_percentage,quarterback_rating,yards_per_game_played,passing_yards_per_attempt,fourth_quarter_comebacks,passing_yards])
+            currentYear += 1
+    skippedFirst = True
+
+f = open('data/qbStats.csv', 'w')
+
+writer = csv.writer(f)
+writer.writerow(["QB Name","Year", "Completed Passes","Attempted Passes","Catch Percentage","Passing Touchdowns","Interceptions Thrown","Passing Yards per Attempt","Interception Percentage","Quarterback Rating","Yards Per Game Played","Passing Yards per Attempt","Fourth Quarter Comebacks","Passing Yards"])
+
+for stat in stats:
+    writer.writerow(stat)
+
+
+    # stats needed - cmp, att, cmp%, yds, td, int, int%, 1D, rate, y/a, gwd, awards
+
+
