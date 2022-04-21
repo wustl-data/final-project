85051e98d42a22c5d75e6e0ea82fff9c03355171
Ram Damodaran <dramvignesh@gmail.com>
2022-04-20 20:31:41 -0500

    fixed bug with dak prescott

--- final-project/final_project/getQbStatsApi.py
+++ final-project/final_project/getQbStatsApi.py
@@ -9,32 +9,27 @@ dataset = pd.read_csv("data/salaryData.csv", header=None)
 skippedFirst = False
 stats = []
 for row_index, row in dataset.iterrows():
-   # print(row[0])
-   # print(row[5])
+
     if(skippedFirst):
         names = row[0].split(" ")
         firstName = names[0]
         lastName = names[1]
-
+    
         number = "00"
         if(row[0] == "Derek Carr" or row[0] == "Josh Allen"):
             number = "02"
+        if(row[0] == "Dak Prescott"):
+            number = "01"
+            
         apiId = lastName[0:4] + firstName[0:2] + number
-     #   print(apiId)
+        
         player = Player(apiId)
-       # print(player.name)  # Prints 'Drew Brees'
-      #  print(player.passing_yards)  # Prints Brees' career passing yards
-        # Prints a Pandas DataFrame of all relevant stats per season for Brees
-        #print(player.dataframe)
         dates = row[5].split("-")
         currentYear = int(dates[0])
         while(currentYear <= int(dates[1])):
             
             completed_passes = player(str(currentYear)).completed_passes
             attempted_passes = player(str(currentYear)).attempted_passes 
-            #catch_percentage = player(str(currentYear)).catch_percentage
-            #print(catch_percentage)
-            
             passing_touchdowns = player(str(currentYear)).passing_touchdowns
             interceptions_thrown = player(str(currentYear)).interceptions_thrown
             passing_yards_per_attempt = player(str(currentYear)).passing_yards_per_attempt
@@ -42,9 +37,6 @@ for row_index, row in dataset.iterrows():
             quarterback_rating = player(str(currentYear)).quarterback_rating
             yards_per_game_played = player(str(currentYear)).yards_per_game_played
             passing_yards_per_attempt = player(str(currentYear)).passing_yards_per_attempt
-           # fourth_quarter_comebacks = player(str(currentYear)).fourth_quarter_comebacks
-           # if(fourth_quarter_comebacks is None):
-           #     fourth_quarter_combacks = 0
             passing_yards = player(str(currentYear)).passing_yards
 
             newRow = [row[0],currentYear,completed_passes,attempted_passes,passing_touchdowns,interceptions_thrown,passing_yards_per_attempt,interception_percentage,quarterback_rating,yards_per_game_played,passing_yards_per_attempt,passing_yards]
@@ -62,6 +54,3 @@ writer.writerow(["QB Name","Year", "Completed Passes","Attempted Passes","Passin
 
 for stat in stats:
     writer.writerow(stat)
-
-
-    # stats needed - cmp, att, cmp%, yds, td, int, int%, 1D, rate, y/a, gwd, awards
