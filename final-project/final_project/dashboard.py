bade003d3f5fe0c9150e594d98ab11fd18fe806b
mattm0606 <matt.m@wustl.edu>
2022-05-10 17:38:24 -0500

    Matt code

--- final-project/final_project/dashboard.py
+++ final-project/final_project/dashboard.py
@@ -99,9 +99,22 @@ app.layout = html.Div([
         ]),
         dcc.Tab(label='Game Impact', children =[
             dcc.Graph(id='completions_full'),
-            dcc.Checklist(completed_passes_df['down'].unique,['1'], id = 'completions_full_check'),
+            dcc.Checklist(completed_passes_df['down'].unique(),[1], id = 'completions_full_check'),
+            dcc.Graph(id='completion_desc'),
+            dcc.RadioItems(
+                id="select_stat",
+                options =[{"label": i, "value": i} for i in comp_desc.columns[1:]],
+                value=comp_desc.columns[1],
+            ),
             dcc.Graph(id ='incompletions_full'),
-            dcc.Checklist(completed_passes_df['down'].unique,['1'], id = 'incompletions_full_check'),
+            dcc.Checklist(completed_passes_df['down'].unique(),[1], id = 'incompletions_full_check'),
+            dcc.Graph(id='incompletion_desc'),
+            dcc.RadioItems(
+                id="in_select_stat",
+                options =[{"label": i, "value": i} for i in incomp_desc.columns[1:]],
+                value=incomp_desc.columns[1],
+            ),
+
         ])
     ])
 ])
@@ -241,6 +254,29 @@ def render_content(graph):
         ])
 
 #Start of Matt's GameImpact Tab
+@app.callback(Output('completions_full', 'figure'),Input('completions_full_check','value'))
+
+def completions_full_graph(downs):
+    filter_df = completed_passes_df[completed_passes_df['down'].isin(downs)]
+    filter_df['string_down'] = filter_df['down'].astype(str)
+    return px.scatter(filter_df, x='playResult', y='epaYards', color ='string_down', symbol ='down', labels={'playResult':'Yards Gained on Play', 'epaYards':'Game Impact Negative', 'down':'Down', 'string_down': 'Down'})
+
+@app.callback(Output('completion_desc','figure'),Input('select_stat', 'value'))
+def completion_desc(radio):
+    df = comp_desc.reset_index()
+    return px.bar(df, x='possessionTeam', y=radio)
+
+
+@app.callback(Output('incompletions_full', 'figure'),Input('incompletions_full_check','value'))
+def incompletions_full_graph(downs):
+    filter_df = incompleted_passes_df[incompleted_passes_df['down'].isin(downs)]
+    filter_df['string_down'] = filter_df['down'].astype(str)
+    return px.scatter(filter_df, x='absoluteYardlineNumber', y='epaYardsOp', color ='string_down', symbol ='down', labels={'absoluteYardlineNumber':'Yards to End of Endzone', 'epaYardsOp':'Game Impact Positive', 'down':'Down', 'string_down': 'Down'})
+
+@app.callback(Output('incompletion_desc','figure'),Input('in_select_stat', 'value'))
+def completion_desc(radio):
+    df = incomp_desc.reset_index()
+    return px.bar(df, x='possessionTeam', y=radio)
 
 #End of Matt's GameImpact Tab
 
